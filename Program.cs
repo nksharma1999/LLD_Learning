bed99fdfe04beb601a1907df097718fa563ff988
Nandkishor Sharma <Nandkishor.Sharma@SuzlonEnergyAS.onmicrosoft.com>
2026-06-19 12:18:18 +0530

    Running Code

--- Program.cs
+++ Program.cs
@@ -5,15 +5,15 @@ class Program
 {
     static void Main()
     {
-        // SInitialization sInitialization = new SInitialization();
-        // sInitialization.Initialize();
-        // OInitialization oInitialization = new OInitialization();
-        // oInitialization.Initialization();
-        // LInitialization lInitialization = new LInitialization();
-        // lInitialization.Initialization();
-        // IInitialization iInitialization = new IInitialization();
-        // iInitialization.Initialize();
-        // DInitialization dInitialization = new DInitialization();
-        // dInitialization.Initialize();
+        SInitialization sInitialization = new SInitialization();
+        sInitialization.Initialize();
+        OInitialization oInitialization = new OInitialization();
+        oInitialization.Initialization();
+        LInitialization lInitialization = new LInitialization();
+        lInitialization.Initialization();
+        IInitialization iInitialization = new IInitialization();
+        iInitialization.Initialize();
+        DInitialization dInitialization = new DInitialization();
+        dInitialization.Initialize();
     }
 }
